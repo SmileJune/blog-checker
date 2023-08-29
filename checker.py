a231f563d4a597b1348e1246ef1c9dc514f555ea
Smilejune <godhkekf24@inha.edu>
2023-08-29 12:48:33 +0900

    feat: 초기 세팅

---
+++ checker.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import requests
+
+def print_world():
+    return "Hello World!"
---
+++ test_print.py
@@ -0,0 +1,4 @@
+from checker import print_world
+
+def test_print_world():
+    assert print_world() == "Hello World!"
